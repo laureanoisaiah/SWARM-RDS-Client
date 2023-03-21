f44f399c0f8be4e88c27c6e15ca878d3a2b980b4
Tyler <tyler.fedrizzi@gmail.com>
2023-03-21 12:00:53 -0400

    Update script for running container

--- utils/run_swarm_container.sh
+++ utils/run_swarm_container.sh
@@ -5,11 +5,12 @@
 # offline validation of License Keys, which is mounted into the container
 # at runtime.
 
-if [ -d ".cache" ]
+if [ -d ".cache" ]:
+then
     echo "Directory .cache exists."
 else
     echo "Creating .cache directory"
     $_mkdir -p ".cache"
 fi
 
-docker run -it --rm --gpus=all --runtime=nvidia --network=host -v $pwd.cache:/home/airsim_user/SWARMCore/core/.cache swarm_home_v1.5.0
+docker run -it --rm --gpus=all --runtime=nvidia --network=host -v $pwd.cache:/home/airsim_user/SWARMCore/core/.cache $1
